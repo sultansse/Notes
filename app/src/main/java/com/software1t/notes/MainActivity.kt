3266db14dcfbd744aa9b81d4804fbbcc93281449
Sultan <ss.softwareit@gmail.com>
2023-01-05 15:43:45 +0600

    deleted toolbar

--- app/src/main/java/com/software1t/notes/MainActivity.kt
+++ app/src/main/java/com/software1t/notes/MainActivity.kt
@@ -2,7 +2,6 @@ package com.software1t.notes
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
