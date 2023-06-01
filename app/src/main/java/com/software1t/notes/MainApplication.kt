1d724918143738d5c61f9f04d28e9340fc309d59
Sultan <ss.softwareit@gmail.com>
2023-06-02 01:02:53 +0600

    added dependencies of koin

--- app/src/main/java/com/software1t/notes/MainApplication.kt
+++ app/src/main/java/com/software1t/notes/MainApplication.kt
@@ -1,16 +1,22 @@
 package com.software1t.notes
 
+import android.app.Application
+import com.software1t.notes.di.AppModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
 
-//class MainApplication : Application() {
-//
-//    override fun onCreate() {
-//        super.onCreate()
-//
-//        startKoin {
-//            androidLogger(Level.DEBUG)
-//            androidContext(this@MainApplication)
-//            modules(listOf(AppModule/*, DatabaseModule*/))
-//        }
-//
-//    }
-//}
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules(listOf(AppModule/*, DatabaseModule*/))
+        }
+
+    }
+}
--- app/src/main/java/com/software1t/notes/di/AppModule.kt
+++ app/src/main/java/com/software1t/notes/di/AppModule.kt
@@ -1,10 +1,39 @@
 package com.software1t.notes.di
 
-//val AppModule = module {
-//
+import org.koin.dsl.module
+
+val AppModule = module {
+
+}
 //    viewModel { PostsViewModel(get()) }
 //
 //    single { createGetPostsUseCase(get()) }
 //
 //    single { createPostRepository(get()) }
+//
+//
+//
+//
+//val dbModule = module {
+//    single {
+//        NotesDatabase.getInstance(androidContext())
+//    }
+//}
+//
+//val viewModelModule = module {
+//    factory { NewNoteViewModel(get()) }
+//    factory { NotesListViewModel(get()) }
+//    factory { NoteViewViewModel(get()) }
+//}
+//
+//val useCaseModule = module {
+//    factory { NewNoteUseCase(get()) }
+//    factory { GetNotesUseCase(get()) }
+//    factory { FetchNoteByIdUseCase(get()) }
+//}
+//
+//val repositoryModule = module {
+//    single<NotesRepository> {
+//        NotesRepositoryImpl(get())
+//    }
 //}
